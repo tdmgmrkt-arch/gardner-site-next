b03386dd46341b6a3faa60b81f56680e2f5bfd76
kennethlerch <kenneth.lerch@gmail.com>
2026-01-08 12:01:23 -0800

    recaptcha key fix revert

--- components/LeadForm.tsx
+++ components/LeadForm.tsx
@@ -19,7 +19,7 @@ declare global {
   }
 }
 
-const RECAPTCHA_SITE_KEY = "6LcMW0QsAAAAAH5CNz1Xt0yG2GSgfHqBpqK90N11";
+const RECAPTCHA_SITE_KEY = "6LeEL0QsAAAAAOdOJFRc9PxAimmuVchWMVZjBcBk";
 
 export function LeadForm() {
   const [formData, setFormData] = useState({
